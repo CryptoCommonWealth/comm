8bde2ebf1b0a2d78045d9d0e242fa21c7713fb25
Zhiyu Zhang <zzhang@circle.com>
2019-10-21 17:06:40 -0400

    initial test

--- test/CommTokenVesting.test.js
+++ test/CommTokenVesting.test.js
@@ -1,12 +1,28 @@
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const Comm = artifacts.require('./Comm');
 
 const CommTokenVesting = artifacts.require('./CommTokenVesting');
-
 
 require('chai')
     .use(require('chai-as-promised'))
     .should();
 
 contract('CommTokenVesting', function ([_, owner, beneficiary]) {
+    beforeEach(async function () {
+        // +1 minute so it starts after contract instantiation
+        this.start = (await time.latest()).add(time.duration.minutes(1));
+        this.cliffDuration = time.duration.years(1);
+        this.duration = time.duration.years(2);
+    });
 
+    it('reverts with a duration shorter than the cliff', async function () {
+        const cliffDuration = this.duration;
+        const duration = this.cliffDuration;
+        expect(cliffDuration > duration, true);
+
+        await expectRevert(
+            CommTokenVesting.new(beneficiary, this.start, cliffDuration, duration, 0, 0, true, { from: owner }),
+            'TokenVesting: cliff is longer than duration'
+        );
+    });
 });

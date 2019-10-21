e444a8defda966820d2bb580dd8289abb94a4c5f
Zhiyu Zhang <zzhang@circle.com>
2019-10-20 23:19:24 -0400

    fix space

--- contracts/CommTokenVesting.sol
+++ contracts/CommTokenVesting.sol
@@ -61,8 +61,8 @@ contract CommTokenVesting is Ownable {
         // solhint-disable-next-line max-line-length
         require(start.add(duration) > block.timestamp, "TokenVesting: final time is before current time");
 
-        require( unlockParam0 >= 0 && unlockParam1 >= 0, "TokenVesting: unlockParam0 or unlockParam1 is negative." );
-        require( duration >= 86400, "TokenVesting: duration must be over a day at least." );
+        require(unlockParam0 >= 0 && unlockParam1 >= 0, "TokenVesting: unlockParam0 or unlockParam1 is negative.");
+        require(duration >= 86400, "TokenVesting: duration must be over a day at least.");
 
         _beneficiary = beneficiary;
         _revocable = revocable;

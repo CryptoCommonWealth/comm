b3b5d9bd35a77630ba019ebf5cae92c2b4f76d87
Zhiyu Zhang <zzhang@circle.com>
2019-11-03 13:18:37 -0500

    add negative tests

--- test/CommTokenVesting.test.js
+++ test/CommTokenVesting.test.js
@@ -1,28 +1,69 @@
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const Comm = artifacts.require('./Comm');
-
 const CommTokenVesting = artifacts.require('./CommTokenVesting');
+const ETH_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const ONE_HUNDRED_MILLION = 100000000;
 
 require('chai')
     .use(require('chai-as-promised'))
     .should();
 
 contract('CommTokenVesting', function ([_, owner, beneficiary]) {
+    let start;
+    let cliffDuration;
+    let duration;
+    let token;
+    let tokenVesting;
+
     beforeEach(async function () {
         // +1 minute so it starts after contract instantiation
-        this.start = (await time.latest()).add(time.duration.minutes(1));
-        this.cliffDuration = time.duration.years(1);
-        this.duration = time.duration.years(2);
+        start = (await time.latest()).add(time.duration.minutes(1));
+        console.log('start: ', start.toString())
+        cliffDuration = time.duration.years(1);
+        console.log('cliffDuration: ', cliffDuration.toString())
+        duration = time.duration.years(2);
+        console.log('duration: ', duration.toString())
+
+        token = await Comm.new();
+        tokenVesting = await CommTokenVesting.new(
+            beneficiary,
+            start,
+            cliffDuration,
+            duration,
+            0,
+            0,
+            true
+        )
     });
 
-    it('reverts with a duration shorter than the cliff', async function () {
-        const cliffDuration = this.duration;
-        const duration = this.cliffDuration;
-        expect(cliffDuration > duration, true);
+    describe('negative tests', async function() {
+        it('reverts with a duration shorter than the cliff', async function () {
+            expect(cliffDuration > duration, true);
 
-        await expectRevert(
-            CommTokenVesting.new(beneficiary, this.start, cliffDuration, duration, 0, 0, true, { from: owner }),
-            'TokenVesting: cliff is longer than duration'
-        );
+            await expectRevert(
+                CommTokenVesting.new(ETH_ZERO_ADDRESS, start, duration, cliffDuration, 0, 0, true, { from: owner }),
+                'TokenVesting: beneficiary is the zero address'
+            );
+
+            await expectRevert(
+                CommTokenVesting.new(beneficiary, start, duration, cliffDuration, 0, 0, true, { from: owner }),
+                'TokenVesting: cliff is longer than duration'
+            );
+        });
+
+        it('reverts immedReleasedAmount is larger than 100000000.', async function() {
+            await expectRevert(
+                CommTokenVesting.new(beneficiary, start, cliffDuration, duration, ONE_HUNDRED_MILLION + 1, 0, true, { from: owner }),
+                'TokenVesting: immedReleasedAmount is larger than 100000000.'
+            );
+        });
+
+        it('reverts dailyReleasedAmount*_durationInDays is larger than 100000000.', async function() {
+            await expectRevert(
+                CommTokenVesting.new(beneficiary, start, cliffDuration, duration, 0, ONE_HUNDRED_MILLION, true, { from: owner }),
+                'TokenVesting: dailyReleasedAmount*_durationInDays is larger than 100000000.'
+            );
+        })
     });
 });
+
